89554b315a51fe70a79ce721578d191b5a77175c
Tony Herrera <tonyeherrera@github.com>
2022-08-28 14:08:44 -0400

    more styling and some placeholder for home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,6 +7,16 @@ function Home(){
     return(
         <div className="home--content">
             <h1 className="title">Welcome</h1>
+            <hr/>
+            <div>
+                <h3>Our Mission</h3>
+                <p>To provide HR heros with tools to help their team succeed</p>
+            </div>
+            <hr/>
+            <div>
+                <h3>About the Site</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra justo nec ultrices dui. Morbi tristique senectus et netus et malesuada. Ultrices dui sapien eget mi proin sed libero. Donec ultrices tincidunt arcu non. Tristique nulla aliquet enim tortor at. Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Ultrices vitae auctor eu augue ut lectus arcu. Massa massa ultricies mi quis hendrerit dolor magna. Gravida neque convallis a cras semper auctor neque. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Mattis aliquam faucibus purus in. Id nibh tortor id aliquet lectus proin nibh nisl. Fermentum odio eu feugiat pretium nibh.</p>
+            </div>
         </div>
     )
 }
